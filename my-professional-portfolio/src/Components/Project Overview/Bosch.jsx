2200fcc354eea8e87fb90181f83a726849a87ad6
Sayem Ibne Taher <sayemtaher788@gmail.com>
2025-10-30 13:45:17 +0100

    minor changes

--- my-professional-portfolio/src/Components/Project Overview/Bosch.jsx
+++ my-professional-portfolio/src/Components/Project Overview/Bosch.jsx
@@ -37,25 +37,39 @@ export const Bosch = () => {
         <div>
           <div>
             {/* {main section} */}
-            <div className="flex flex-col lg:flex-row gap-5 lg:gap-0 w-full  items-center justify-between">
-              <div className="flex gap-2">
-                <span className="text-2xl lg:text-4xl font-bold text-purple-600">
-                  Bosch Security Systems
-                </span>
-                <div className="border-2 flex items-center gap-1 justify-center border-pink-500 py-2 px-4 text-pink-500 rounded-2xl font-bold text-xs">
-                  <FaStar className="text-xl"></FaStar> <span className="text-lg">10/10</span>
+            <div className="flex items-center lg:items-start justify-center lg:justify-start">
+              <img
+                className="rounded-xl  bg-white px-2 mb-2 w-[180px] object-cover"
+                src="/Bosch-Logo.png"
+                alt=""
+              />
+            </div>
+
+            <div className="flex flex-col gap-5    justify-between">
+              <div className="flex items-center lg:flex-row flex-col gap-8 lg:gap-0 justify-between">
+                <div className="flex lg:flex-row flex-col items-center gap-2 justify-center   text-pink-500 rounded-2xl font-bold text-xs">
+                  <span className="text-3xl lg:text-4xl font-bold text-purple-600">
+                    Bosch Security Systems
+                  </span>
+                  <div className="flex gap-2 border-2 rounded-full py-2 border-pink-500 px-4 items-center">
+                    <FaStar className="text-xl"></FaStar>{" "}
+                    <span className="text-lg">10/10</span>
+                  </div>
+                </div>
+
+                <div>
+                  {" "}
+                  <span className="bg-pink-50 text-[16px]  w-[250px] lg:text-sm font-semibold text-pink-500 border-2 border-pink-100 pt-2 pb-2 pl-4 pr-4 rounded-full">
+                    {" "}
+                    February, 2025 - June, 2025
+                  </span>
                 </div>
               </div>
-
-              <span className="bg-pink-50 text-[16px]  w-[250px] lg:text-sm font-semibold text-pink-500 border-2 border-pink-100 pt-2 pb-2 pl-4 pr-4 rounded-full">
-                {" "}
-                February, 2025 - June, 2025
-              </span>
             </div>
 
             <a
               href="#"
-              className="block mt-4 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
+              className="block mt-6 lg:mt-4 text-xl font-semibold text-gray-600 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline"
               tabIndex={0}
               role="link"
             >
@@ -104,9 +118,9 @@ export const Bosch = () => {
             </span>
           </div>
         </div>
-        <div className="mt-10 bg-white p-6 rounded-md">
+        <div className="mt-10  bg-white p-6 rounded-md">
           <span className="text-xl font-bold">Tools & Technologies</span>
-          <div className="flex flex-wrap gap-4 mt-5 items-center">
+          <div className="flex flex-wrap justify-center lg:justify-start gap-4 mt-5 items-center">
             <div className="flex flex-col border-2 border-blue-200 p-2 rounded-xl gap-2 items-center justify-center h-[120px] w-[200px] bg-blue-50 text-blue-600">
               <FaReact className="text-5xl"></FaReact>
               <span className="text-lg text-blue-700 font-bold">React</span>
